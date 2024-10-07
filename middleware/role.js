c8d50bbc814c6441e56c46ca52f6ae986cf41688
amoskefe <amoskefe4@gmail.com>
2024-10-07 02:54:31 +0100

    Resolve merge conflict in role.js

--- middleware/role.js
+++ middleware/role.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(roles) {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
@@ -7,13 +6,3 @@ module.exports = function(roles) {
     next();
   };
 };
-=======
-module.exports = function(roles) {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: 'Access denied' });
-    }
-    next();
-  };
-};
->>>>>>> 7a49a456001b268e5ede99ea44b88b163f8ad76e
