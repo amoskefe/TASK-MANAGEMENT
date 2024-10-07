c3c9ec5aab191da8507c759a3cb1e69db56afd92
amoskefe <amoskefe4@gmail.com>
2024-10-07 03:04:26 +0100

    Update task management API with latest changes

--- Models/Comment.js
+++ Models/Comment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
@@ -23,29 +22,3 @@ const CommentSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
-=======
-const mongoose = require('mongoose');
-
-const CommentSchema = new mongoose.Schema({
-  task: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task',
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-module.exports = mongoose.model('Comment', CommentSchema);
->>>>>>> 7a49a456001b268e5ede99ea44b88b163f8ad76e
--- Models/Notification.js
+++ Models/Notification.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const NotificationSchema = new mongoose.Schema({
@@ -22,28 +21,3 @@ const NotificationSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Notification', NotificationSchema);
-=======
-const mongoose = require('mongoose');
-
-const NotificationSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  message: {
-    type: String,
-    required: true
-  },
-  read: {
-    type: Boolean,
-    default: false
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-module.exports = mongoose.model('Notification', NotificationSchema);
->>>>>>> 7a49a456001b268e5ede99ea44b88b163f8ad76e
