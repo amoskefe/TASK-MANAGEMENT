b2da651b40cd51f40d281ced0c4391535189b12b
amoskefe <amoskefe4@gmail.com>
2024-10-07 02:56:51 +0100

    Resolve merge conflict in upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const multer = require('multer');
 const path = require('path');
 
@@ -28,34 +27,3 @@ const upload = multer({
 });
 
 module.exports = upload;
-=======
-const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.filename + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
-
-module.exports = upload;
->>>>>>> 7a49a456001b268e5ede99ea44b88b163f8ad76e
